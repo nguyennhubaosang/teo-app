99ab6bac3f47933cd10ff0d36ed4a662825f1ed3
nguyennhubaosang <nguyennhubaosang@gmail.com>
2020-02-25 13:37:01 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learning Dev React
         </a>
       </header>
     </div>
