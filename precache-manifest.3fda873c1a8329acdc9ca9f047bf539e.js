33ff21b7bfcc41c855384c85ff03665f4cef319f
nguyennhubaosang <nguyennhubaosang@gmail.com>
2020-02-25 13:17:40 +0700

    Updates

---
+++ precache-manifest.3fda873c1a8329acdc9ca9f047bf539e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f2d5bc4047ded4f8e6542a6133c3a5",
+    "url": "/nguyennhubaosang/teo-app/index.html"
+  },
+  {
+    "revision": "ab7ec265648d5cf11977",
+    "url": "/nguyennhubaosang/teo-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d8261b4b16b415796aed",
+    "url": "/nguyennhubaosang/teo-app/static/js/2.b6d67989.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/nguyennhubaosang/teo-app/static/js/2.b6d67989.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab7ec265648d5cf11977",
+    "url": "/nguyennhubaosang/teo-app/static/js/main.5cebca7e.chunk.js"
+  },
+  {
+    "revision": "f9af31f9df19439f05e9",
+    "url": "/nguyennhubaosang/teo-app/static/js/runtime-main.2b4e8fb8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nguyennhubaosang/teo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
